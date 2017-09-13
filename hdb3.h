6bddbfaa9a7fe1edd27dca1ab7038626bcb17d05
MaPePeR <MaPePeR@users.noreply.github.com>
2017-09-14 00:32:15 +0200

    Implement HDB3Context

--- hdb3.h
+++ hdb3.h
@@ -4,17 +4,53 @@
 class HDB3Context {
 public:
 	inline bool hasOutput() {
-		return true;
+		return output_pattern_pos != 0;
 	}
-	inline void inputData(bool bit) {
-
+	inline bool inputData(bool bit) {
+		assert(!this->hasOutput());
+		if (bit) {
+			output_pattern = last_one_negative ? ZZZP : ZZZN;
+			output_pattern_pos = 1 << current_zero_count;
+			current_zero_count = 0;
+			odd_pairs = !odd_pairs;
+			return true;
+		} else {
+			current_zero_count += 1;
+			if (current_zero_count == 4) {
+				if (odd_pairs) {
+					output_pattern = last_one_negative ? ZZZN : ZZZP;
+					output_pattern_pos = 1 << 3;
+				} else {
+					output_pattern = last_one_negative ? PZZP : NZZN;
+					output_pattern_pos = 1 << 3;
+				}
+				current_zero_count = 0;
+				odd_pairs = 0;
+				return true;
+			}
+		}
+		return false;
 	}
 	inline int getOutput() {
-		return 0;
+		assert(this->hasOutput());
+		int isnz = (output_pattern & output_pattern_pos) > 0;
+		output_pattern_pos >>= 1;
+		if (isnz) {
+			last_one_negative = (output_pattern & 0x10) > 0;
+			return last_one_negative ? -1 : 1;
+		} else {
+			return 0;
+		}
 	}
 	inline void close() {
 
 	}
+protected:
+	int output_pattern_pos = 0;
+	enum {ZZZZ=0x00,PZZP=0x09,NZZN=0x19,ZZZP=0x01,ZZZN=0x11} output_pattern = ZZZZ;
+	int current_zero_count = 0;
+	int last_one_negative = 1;
+	int odd_pairs = 0;
 };
 
 template <int (*func)()>

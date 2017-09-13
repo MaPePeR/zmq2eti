1a4f9caefe521b7c150d39f8116462f8f4af20e8
MaPePeR <MaPePeR@users.noreply.github.com>
2017-09-14 00:53:38 +0200

    Fix HDB3-Test & implement close() & remove outdated implementation

--- hdb3.h
+++ hdb3.h
@@ -8,6 +8,7 @@ public:
 	}
 	inline bool inputData(bool bit) {
 		assert(!this->hasOutput());
+		assert(!is_closed);
 		if (bit) {
 			output_pattern = last_one_negative ? ZZZP : ZZZN;
 			output_pattern_pos = 1 << current_zero_count;
@@ -43,7 +44,12 @@ public:
 		}
 	}
 	inline void close() {
-
+		assert(!this->hasOutput());
+		if (current_zero_count > 0) {
+			output_pattern = ZZZZ;
+			output_pattern_pos = 1 << (current_zero_count - 1);
+		}
+		is_closed = true;
 	}
 protected:
 	int output_pattern_pos = 0;
@@ -51,45 +57,5 @@ protected:
 	int current_zero_count = 0;
 	int last_one_negative = 1;
 	int odd_pairs = 0;
+	bool is_closed = false;
 };
-
-template <int (*func)()>
-int get_next_HDB3() {
-	static int output_pattern_pos = 0;
-	static enum {ZZZZ=0x00,PZZP=0x09,NZZN=0x19,ZZZP=0x01,ZZZN=0x11} output_pattern = ZZZZ;
-	static int current_zero_count = 0;
-	static int last_one_negative = 1;
-	static int odd_pairs = 0;
-
-	while (output_pattern_pos == 0) {
-		int next_bit = func();
-		if (next_bit) {
-			output_pattern = last_one_negative ? ZZZP : ZZZN;
-			output_pattern_pos = 1<<current_zero_count;
-			current_zero_count = 0;
-			odd_pairs = !odd_pairs;
-		} else {
-			current_zero_count += 1;
-			if (current_zero_count == 4) {
-				if (odd_pairs) {
-					output_pattern = last_one_negative ? ZZZN : ZZZP;
-					output_pattern_pos = 1<<3;
-				} else {
-					output_pattern = last_one_negative ? PZZP : NZZN;
-					output_pattern_pos = 1<<3;
-				}
-				current_zero_count = 0;
-				odd_pairs = 0;
-			}
-		}
-	}
-	assert (output_pattern_pos > 0);
-	int isnz = (output_pattern & output_pattern_pos) > 0;
-	output_pattern_pos >>= 1;
-	if (isnz) {
-		last_one_negative = (output_pattern & 0x10) > 0;
-		return last_one_negative ? -1 : 1;
-	} else {
-		return 0;
-	}
-}
--- hdb3_test.cpp
+++ hdb3_test.cpp
@@ -8,38 +8,28 @@ constexpr const char *testdata[]= {
 	"1010000100001100001110000111100001010000"
 };
 
-#define GET_DATA(i) \
-int get_data_ ## i () { \
-	static int pos = 0; \
-	if (pos > strlen(testdata[i])) return pos++ & 1; \
-	return testdata[i][pos++] == '1'; \
-}
-
-GET_DATA(0)
-GET_DATA(1)
-GET_DATA(2)
-
 const char *expected[] = {
 	"+000+−+0",
 	"+0−+00+0−+−00−+−+00+000−",
 	"+0−+00+−000−+−+00+−+−000−+−+−+00+-0+-00-"
 };
 
-#define TEST_FUNCTION(N) \
-void test_function_ ## N() {   \
-	printf("%s\n", testdata[N]); \
-	for (int j = 0; j < strlen(testdata[N]); j++) { \
-		int hdb3 = get_next_HDB3<get_data_ ## N>(); \
-		printf("%c", hdb3 > 0 ? '+' : (hdb3 < 0 ? '-' : '0')); \
-	} \
-	printf("\n%s\n", expected[N]);  \
-}
-TEST_FUNCTION(0)
-TEST_FUNCTION(1)
-TEST_FUNCTION(2)
 int main() {
-	test_function_0();
-	test_function_1();
-	test_function_2();
-	
+	for (int i = 0; i < testcases; i++) {
+		printf("%s\n", testdata[i]);
+		HDB3Context ctx = HDB3Context();
+		for (int j = 0; j < strlen(testdata[i]); j++) {
+			ctx.inputData(testdata[i][j] == '1');
+			while(ctx.hasOutput()) {
+				int hdb3 = ctx.getOutput();
+				printf("%c", hdb3 > 0 ? '+' : (hdb3 < 0 ? '-' : '0'));
+			}
+		}
+		ctx.close();
+		while(ctx.hasOutput()) {
+			int hdb3 = ctx.getOutput();
+			printf("%c", hdb3 > 0 ? '+' : (hdb3 < 0 ? '-' : '0'));
+		}
+		printf("\n%s\n", expected[i]);
+	}	
 }
